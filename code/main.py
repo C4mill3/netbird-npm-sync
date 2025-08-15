1ae30cc9444f47b7d3d4913abccc63d95f134e08
cam <cam@qwerty>
2025-08-16 00:46:05 +0200

    temp debug

--- code/main.py
+++ code/main.py
@@ -41,6 +41,8 @@ if __name__=='__main__':
     npm_token_expires=0
     envs=utility.load_environ()
 
+    main(envs, verbose=True)
+    
     try:
         utility.print_logs("Running initial run...")
         main(envs, verbose=True)
